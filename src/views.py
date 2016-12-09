61ed1408dbb0aed1ac47074a3fd5e8b29f88136e
Yue Zhang <deyomizy@gmail.com>
2016-12-08 21:42:51 -0800

    basic configuration

--- src/views.py
+++ src/views.py
@@ -1,7 +1,39 @@
-from src import app
+from flask import (
+    render_template,
+    redirect,
+    url_for
+)
+from flask_login import current_user
+from src import app, db, login_manager
+from .models import User
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           posts=posts)
+
+
+@app.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
